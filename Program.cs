8211120fb99405e738c179d85d9634a3ea4cea6a
Hatice AKPOLAT <Hatice AKPOLAT@DESKTOP-M2DIBND>
2023-10-31 14:52:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Metotlar;
+using System.Security.Claims;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string urunAdi = "elma";
+        double urunFiyati = 15;
+        string aciklama = "Amasya elması";
+
+        Urun urun1= new Urun();//bir class tanımlamak için oluşturulan örnek yapı
+        urun1.Adi = "Elma";
+        urun1.Fiyati = 15;
+        urun1.Aciklama = "Amasya Elması";
+
+        Urun urun2=new Urun();
+        urun2.Adi = "Çilek";
+        urun2.Fiyati = 20;
+        urun2.Aciklama = "Çilek gibi çilek";
+        Urun[] urunler= new Urun[] {urun1, urun2};
+        foreach (Urun urun in urunler)
+        {
+            Console.WriteLine("Ürün Adı:" + urun.Adi);
+            Console.WriteLine("Ürün Fiyatı:" + urun.Fiyati);
+            Console.WriteLine("Ürün Açıklaması:" + urun.Aciklama);
+            Console.WriteLine("---------------------------");
+        }
+        Console.WriteLine("----------Metotlar----------");
+        //instance ----> örneğini oluşturma işlemi 
+        SepetManager sepetManager = new SepetManager();
+        sepetManager.Ekle(urun1);
+        sepetManager.Ekle(urun2);
+        
+
+    }
+}
---
+++ SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //Naming convention --> isimlendirme kuralları Metotlar isimlendirilirken büyük harfle yazılır.
+        public void Ekle(Urun urun)
+        {
+
+            Console.WriteLine("Tebrikler.Sepete eklendi:"+urun.Adi);
+        }
+    }
+}
